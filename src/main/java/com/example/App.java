284d3256f6d6f404a1c111b89c80263cda84fe92
Prithviraj <147900126+prithvicatalog@users.noreply.github.com>
2026-04-14 12:56:27 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,52 +2,72 @@ package com.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType; // Required for new tabs
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
 
 public class App {
-    public static void main(String[] args) {
-        FirefoxOptions options = new FirefoxOptions();
-        
-        // Ensure we are NOT in headless mode so you can see the new tab
-        // options.addArguments("-headless"); 
 
+    public static void main(String[] args) throws InterruptedException {
+
+        FirefoxOptions options = new FirefoxOptions();
         options.setBinary("/home/vboxuser/Desktop/lab1/firefox/firefox");
-        options.addArguments("--headless=new");       
-        options.addArguments("--no-sandbox");         
-        options.addArguments("--disable-dev-shm-usage"); 
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
-        options.addArguments("--window-size=1920,1080");
 
         WebDriver driver = new FirefoxDriver(options);
+        driver.manage().window().setSize(new org.openqa.selenium.Dimension(1920, 1080));
 
-        try {
-            // 1. Login Process
-            driver.get("https://www.saucedemo.com/");
-            driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        Actions actions = new Actions(driver);
 
-            driver.findElement(By.id("user-name")).sendKeys("standard_user");
-            driver.findElement(By.id("password")).sendKeys("secret_sauce");
-            driver.findElement(By.id("login-button")).click();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        System.out.println("SauceDemo login successful");
 
-            System.out.println("Login successful. Opening new tab...");
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://automationexercise.com/products");
+        driver.findElement(By.id("search_product")).sendKeys("Men Tshirt");
+        driver.findElement(By.id("submit_search")).click();
 
-            // 2. Open a NEW TAB and switch focus to it
-            driver.switchTo().newWindow(WindowType.TAB);
+        WebElement product = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[data-product-id='2']"))
+        );
 
-            // 3. Navigate to a different site in the new tab
-            driver.get("https://www.google.com");
-            
-            System.out.println("New tab opened. Script finished, but browser will stay open.");
+        ((org.openqa.selenium.JavascriptExecutor) driver)
+                .executeScript("arguments[0].scrollIntoView({block:'center'});", product);
+        ((org.openqa.selenium.JavascriptExecutor) driver)
+                .executeScript("arguments[0].click();", product);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            // Even if it errors, we want to keep it open for debugging
-        }
+        WebElement viewCart = wait.until(
+                ExpectedConditions.elementToBeClickable(By.cssSelector("#cartModal a[href='/view_cart']"))
+        );
+        ((org.openqa.selenium.JavascriptExecutor) driver)
+                .executeScript("arguments[0].scrollIntoView(true);", viewCart);
+        ((org.openqa.selenium.JavascriptExecutor) driver)
+                .executeScript("arguments[0].click();", viewCart);
 
-        // 4. REMOVED driver.quit() 
-        // Note: The Java program will finish executing, 
-        // but the Firefox process will remain active on your desktop.
+        System.out.println("Automation Exercise product added to cart");
+
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        Thread.sleep(2000);
+        driver.findElement(By.id("username")).sendKeys("student");
+        driver.findElement(By.id("password")).sendKeys("Password123");
+        driver.findElement(By.id("submit")).click();
+        System.out.println("Practice Test Automation login successful");
+
+        Thread.sleep(5000);
+        
     }
 }
